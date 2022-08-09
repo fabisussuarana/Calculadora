e86f74fe44f157d9a42f89644ee6f7b4a8e89c42
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2022-08-09 19:59:40 -0300

    #2 Adicionar os operadores básicos (subtração, multiplicação e divisão)

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,10 @@
 number_1 = int(input('Digite o primeiro número: '))
 number_2 = int(input('Digite o segundo número: '))
 
-print('{} + {} = '.format(number_1, number_2))
-print(number_1 + number_2) 
+print(f'{number_1} + {number_2} =' number_1 + number_2)
+
+print(f'{number_1} - {number_2} =' number_1 - number_2)
+
+print(f'{number_1} * {number_2} =' number_1 * number_2)
+
+print(f'{number_1} / {number_2} =' number_1 / number_2)
