eb944006aededaa5fe78ce95809f3d7758c6b4db
fabisussuarana <106176566+fabisussuarana@users.noreply.github.com>
2022-08-10 10:14:51 -0500

    #3 Permitir que o usuário selecione a operação que deseja

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,23 @@
-number_1 = int(input('Digite o primeiro número: '))
-number_2 = int(input('Digite o segundo número: '))
+from codecs import oem_decode
+from string import octdigits
 
-print(f'{number_1} + {number_2} =' number_1 + number_2)
 
-print(f'{number_1} - {number_2} =' number_1 - number_2)
+operation = input('''\nPor favor digite a operação matemática que você gostaria de completar 
++ para adição
+- para subtração
+* para multiplicação
+/ para divisão\n\n Operação escolhida: ''')
 
-print(f'{number_1} * {number_2} =' number_1 * number_2)
+number_1 = int(input('\nDigite o primeiro número: '))
+number_2 = int(input('\nDigite o segundo número: '))
 
-print(f'{number_1} / {number_2} =' number_1 / number_2)
+if(operation == '+'):
+    print(f'\n{number_1} + {number_2} =', number_1 + number_2)
+elif(operation == '-'):
+    print(f'\n{number_1} - {number_2} =', number_1 - number_2)
+elif(operation == '*'):
+    print(f'\n{number_1} * {number_2} =', number_1 * number_2)
+elif(operation == '/'):
+    print(f'\n{number_1} / {number_2} =', number_1 / number_2)
+else:
+    print('\nVocê não digitou um operador válido, execute o programa novamente!')
